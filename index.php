2caf6c2056eb50acfff31b28c47a55ac3f30fd6f
Nikolay Migunov <truenikolas@gmail.com>
2017-09-21 13:32:35 +0300

    Выгрузил данные из БД, сделал фильтр по названию и автору

--- index.php
+++ index.php
@@ -2,9 +2,45 @@
 
   require_once "db-connector.php";
 
-  $myFirstRequest = "SELECT * FROM 'books'";
-  $statement = $connect->prepare($myFirstRequest);
-  $statement->execute();
+  if (!empty($_GET)) {
+    $bookName = $_GET["name"];
+    $bookAuthor = $_GET["author"];
+    $bookIsbn = $_GET["isbn"];
+  }
+
+//    if (!empty($bookName)) {
+//      $myFirstRequest = "SELECT * FROM `books` WHERE `name` LIKE ?";
+//      $statement = $connect->prepare($myFirstRequest);
+//      $statement->execute(["%$bookName%"]);
+//    } else {
+//      $myFirstRequest = "SELECT * FROM `books`";
+//      $statement = $connect->prepare($myFirstRequest);
+//      $statement->execute();
+//    }
+
+  $filter = "filter";
+
+  switch ($filter) {
+    case !empty($bookName):
+      $myFirstRequest = "SELECT * FROM `books` WHERE `name` LIKE ?";
+      $statement = $connect->prepare($myFirstRequest);
+      $statement->execute(["%$bookName%"]);
+      break;
+    case !empty($bookAuthor):
+      $myFirstRequest = "SELECT * FROM `books` WHERE `author` LIKE ?";
+      $statement = $connect->prepare($myFirstRequest);
+      $statement->execute(["%$bookAuthor%"]);
+      break;
+    case !empty($bookIsbn):
+      $myFirstRequest = "SELECT * FROM `books` WHERE `isbn` LIKE ?";
+      $statement = $connect->prepare($myFirstRequest);
+      $statement->execute(["%$bookIsbn]%"]);
+      break;
+    default:
+      $myFirstRequest = "SELECT * FROM `books`";
+      $statement = $connect->prepare($myFirstRequest);
+      $statement->execute();
+  }
 
   $results = [];
 
@@ -12,21 +48,9 @@
     $results[] = $row;
   }
 
-echo "<pre>";
-print_r($connect);
-echo "</pre>";
-
-echo "<pre>";
-var_dump($myFirstRequest);
-echo "</pre>";
-
-echo "<pre>";
-var_dump($statement);
-echo "</pre>";
-
-echo "<pre>";
-var_dump($results);
-echo "</pre>";
+//echo "<pre>";
+//var_dump($results);
+//echo "</pre>";
 
 ?>
 
@@ -35,11 +59,20 @@ echo "</pre>";
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <link rel="stylesheet" href="">
+  <link href="https://fonts.googleapis.com/css?family=PT+Serif:400,700&amp;subset=cyrillic" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
   <title>Библиотека успешного человека</title>
 </head>
 <body>
 <div class="wrapper">
+    <form action="">
+        <input type="text" name="author" placeholder="Поиск по автору">
+        <input type="text" name="name" placeholder="Поиск по названию книги">
+        <input type="text" name="isbn" placeholder="Поиск по коду ISBN">
+        <input type="submit" value="Искать">
+    </form>
+
+
   <table>
     <tr>
       <th>Название</th>
@@ -49,11 +82,13 @@ echo "</pre>";
       <th>ISBN</th>
     </tr>
     <?php foreach ($results as $row): ?>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <tr>
+          <td><?php echo $row["name"] ?></td>
+          <td><?php echo $row["author"] ?></td>
+          <td><?php echo $row["year"] ?></td>
+          <td><?php echo $row["genre"] ?></td>
+          <td class="isbn"><?php echo $row["isbn"] ?></td>
+      </tr>
     <?php endforeach; ?>
   </table>
 </div>
